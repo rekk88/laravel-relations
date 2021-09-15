b938c022e2c67a00caee12a4b709e5ae360d3a88
maicol <maicol.matteoli@gmail.com>
2021-09-15 18:01:17 +0200

    add : relazione one to many Category->post

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'categories';
+
+    public function posts(){
+      // una categoria può avere tanti post
+      return $this->hasMany('App\Post');
+    }
 }
